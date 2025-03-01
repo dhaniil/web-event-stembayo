322421df43b34ce8d316eacc6acc37eef60b4045
dhaniil <pemudakrinj20394@gmail.com>
2025-03-01 20:36:32 +0700

    Save changes before pulling updates

--- routes/auth.php
+++ routes/auth.php
@@ -61,7 +61,11 @@ Route::middleware('auth')->group(function () {
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
+<<<<<<< HEAD
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+=======
+    Route::delete('/auth/logout', [AuthenticatedSessionController::class, 'destroy'])
+>>>>>>> 85bb9ea (Save changes before pulling updates)
         ->name('logout');
     Route::delete('/auth/logout', [AuthenticatedSessionController::class, 'destroy']);
 
